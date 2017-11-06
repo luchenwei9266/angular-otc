b7ce9b5481145a2b7fdd31e1e9f236988f7344e5
lu <luchenwei9266@users.noreply.github.com>
2017-11-06 17:49:49 +0800

    配置路由，分离top-menu组件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,37 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+
+const appRoutes: Routes = [
+  { 
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: 'home', component: HomeComponent },
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    TopMenuComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     BrowserAnimationsModule,
-    NgZorroAntdModule.forRoot()
+    NgZorroAntdModule.forRoot(),
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   bootstrap: [AppComponent]
 })
