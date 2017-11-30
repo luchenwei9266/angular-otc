2317964b6cee461c5a8e76719d268ef92dcd480d
lu <luchenwei9266@users.noreply.github.com>
2017-11-30 17:34:55 +0800

    增加过场动画

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { routeAnimation } from './app-animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [routeAnimation]
 })
 export class AppComponent {
-  title = 'app';
+  title="app";
+  // router跳转动画所需参数
+  routerState:boolean = true;
+  routerStateCode:string = 'active';
+  constructor(private router:Router){
+      this.router.events.subscribe(event => {
+        if (event instanceof NavigationEnd) {
+        // 每次路由跳转改变状态
+        this.routerState = !this.routerState;
+        this.routerStateCode = this.routerState ? 'active' : 'inactive';
+        }
+      });
+  }
 }
